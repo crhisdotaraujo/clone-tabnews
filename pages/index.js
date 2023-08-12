670729bb5c2b4325d1784c5efda80a0a31a04557
Crhistian Araujo <113248738+crhisdotaraujo@users.noreply.github.com>
2023-08-12 17:44:22 +0000

    Acrescentando arquivo de página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Quem divide seus objetivos na menor unidade possível no presente, acaba multiplicando exponencialmente o seu resultado no futuro.</h1>
+}
+
+export default Home;
