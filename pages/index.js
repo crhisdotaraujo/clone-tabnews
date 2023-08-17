e7fd9383b9b24c9fe9da38f6ae41b9c53057582a
Crhistian Araujo <113248738+crhisdotaraujo@users.noreply.github.com>
2023-08-17 20:51:14 +0000

    Acrescentando scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Quem <em>divide</em> seus objetivos na menor unidade possível no presente, acaba <em>multiplicando exponencialmente</em> o seu resultado no futuro.</h1>
+  return (
+    <h1>
+      Quem <em>divide</em> seus objetivos na menor unidade possível no presente,
+      acaba <em>multiplicando exponencialmente</em> o seu resultado no futuro.
+    </h1>
+  );
 }
 
 export default Home;
